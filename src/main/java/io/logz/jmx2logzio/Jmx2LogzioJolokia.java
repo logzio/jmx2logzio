05faae215722f2f19833b779b55d159df07234de
yyyogev <yogev.metzuyanim@logz.io>
2019-01-03 16:38:45 +0200

    first commit

---
+++ src/main/java/io/logz/jmx2logzio/Jmx2LogzioJolokia.java
@@ -0,0 +1,15 @@
+package io.logz.jmx2logzio;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+
+public class Jmx2LogzioJolokia {
+    private static final Logger logger = LoggerFactory.getLogger(Jmx2LogzioJolokia.class);
+
+    public static void main(String[] args) {
+        Config config = ConfigFactory.load();
+
+    }
+}
