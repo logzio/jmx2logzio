447a393c8ba2f5ea219b32b8689778b21659fcc6
yyyogev <yogev.metzuyanim@logz.io>
2019-01-06 17:09:35 +0200

    Added optional parameters

--- src/main/java/io/logz/jmx2logzio/objects/LogzioJavaSender.java
+++ src/main/java/io/logz/jmx2logzio/objects/LogzioJavaSender.java
@@ -1,11 +1,34 @@
 package io.logz.jmx2logzio.objects;
 
+import io.logz.sender.LogzioSender;
+
+import java.io.File;
+
 public class LogzioJavaSender {
 
     private String url = "https://listener.logz.io:8071";
     private String type = "jmx2LogzioType";
     private String Token;
     private int threadPoolSize = 3;
+    private boolean debug = true;
+    private boolean compressRequests = true;
+
+    // In-memory queue parameters
+    private int  inMemoryQueueCapacityInBytes = 1024*1024*100;
+    private int logsCountLimit = -1;
+    private int diskSpaceCheckInterval = 1000;
+
+    // Disk queue parameters
+    private File queueDir;
+    private int fileSystemFullPercentThreshold = 98;
+    private int gcPersistedQueueFilesIntervalSeconds = 30;
+
+    public LogzioJavaSender() {
+        String queuePath = System.getProperty("user.dir");
+        queuePath += queuePath.endsWith("/") ? "" : "/";
+        queuePath += "logs";
+        this.queueDir = new File(queuePath);
+    }
 
     public String getUrl() {
         return url;
@@ -32,4 +55,59 @@ public class LogzioJavaSender {
         return threadPoolSize;
     }
 
+    public boolean isDebug() {
+        return debug;
+    }
+
+    public boolean isCompressRequests() {
+        return compressRequests;
+    }
+
+    public int getInMemoryQueueCapacityInBytes() {
+        return inMemoryQueueCapacityInBytes;
+    }
+
+    public void setInMemoryQueueCapacityInBytes(int inMemoryQueueCapacityInBytes) {
+        this.inMemoryQueueCapacityInBytes = inMemoryQueueCapacityInBytes;
+    }
+
+    public int getLogsCountLimit() {
+        return logsCountLimit;
+    }
+
+    public void setLogsCountLimit(int logsCountLimit) {
+        this.logsCountLimit = logsCountLimit;
+    }
+
+    public int getDiskSpaceCheckInterval() {
+        return diskSpaceCheckInterval;
+    }
+
+    public void setDiskSpaceCheckInterval(int diskSpaceCheckInterval) {
+        this.diskSpaceCheckInterval = diskSpaceCheckInterval;
+    }
+
+    public File getQueueDir() {
+        return queueDir;
+    }
+
+    public void setQueueDir(File queueDir) {
+        this.queueDir = queueDir;
+    }
+
+    public int getFileSystemFullPercentThreshold() {
+        return fileSystemFullPercentThreshold;
+    }
+
+    public void setFileSystemFullPercentThreshold(int fileSystemFullPercentThreshold) {
+        this.fileSystemFullPercentThreshold = fileSystemFullPercentThreshold;
+    }
+
+    public int getGcPersistedQueueFilesIntervalSeconds() {
+        return gcPersistedQueueFilesIntervalSeconds;
+    }
+
+    public void setGcPersistedQueueFilesIntervalSeconds(int gcPersistedQueueFilesIntervalSeconds) {
+        this.gcPersistedQueueFilesIntervalSeconds = gcPersistedQueueFilesIntervalSeconds;
+    }
 }
