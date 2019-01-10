7665317993a864bb8a4e0d396ab9aae72466ebc0
yyyogev <yogev.metzuyanim@logz.io>
2019-01-10 14:27:33 +0200

    Changed metrics to match logz.io json format

--- src/main/java/io/logz/jmx2logzio/objects/Metric.java
+++ src/main/java/io/logz/jmx2logzio/objects/Metric.java
@@ -1,12 +1,12 @@
 package io.logz.jmx2logzio.objects;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Metric {
@@ -14,9 +14,17 @@ public class Metric {
     public static final String SERVICE_NAME = "serviceName";
     public static final String SERVICE_HOST = "serviceHost";
     public static final String DOMAIN_NAME = "domainName";
+    private String timestamp;
+
+    @JsonProperty("dimensions")
+    private Map<String, Object> dimensionsMap;
+
+    @JsonProperty("metrics")
+    private Map<String, Number> metricMap;
+
+    @JsonIgnore
     private String name;
     private Number value;
-    private String timestamp;
     private List<Dimension> dimensions;
 
     public Metric() {
@@ -28,12 +36,15 @@ public class Metric {
         this.value = value;
         this.timestamp = timestampFormatter.format(timestamp);
         this.dimensions = dimensions.stream().collect(Collectors.toList());
+        this.dimensionsMap = getDimensionsMap();
+        this.metricMap = getMetricMap();
     }
 
     public String getName() {
         return name;
     }
 
+    @JsonIgnore
     public Number getValue() {
         return value;
     }
@@ -42,6 +53,7 @@ public class Metric {
         return timestamp;
     }
 
+    @JsonIgnore
     public List<Dimension> getDimensions() {
         return dimensions;
     }
@@ -56,20 +68,34 @@ public class Metric {
                 '}';
     }
 
-    @JsonIgnore
-    public String getFullName(){
-        StringBuilder sb = new StringBuilder();
-        dimensions.stream().forEach(dimension -> {
-            String key = dimension.getKey();
-            if(!Arrays.asList(SERVICE_NAME, SERVICE_HOST, DOMAIN_NAME).contains(key)){
-                sb.append(key);
-                sb.append("_");
-            }
-            sb.append(dimension.getValue());
-            sb.append(".");
-        });
-        sb.append(getName());
-        return sb.toString();
+//    @JsonIgnore
+//    public String getFullName(){
+//        StringBuilder sb = new StringBuilder();
+//        dimensions.stream().forEach(dimension -> {
+//            String key = dimension.getKey();
+//            if(!Arrays.asList(SERVICE_NAME, SERVICE_HOST, DOMAIN_NAME).contains(key)){
+//                sb.append(key);
+//                sb.append("_");
+//            }
+//            sb.append(dimension.getValue());
+//            sb.append(".");
+//        });
+//        sb.append(getName());
+//        return sb.toString();
+//    }
+
+    public Map<String, Object> getDimensionsMap() {
+        Map<String, Object> jsonDimensions = new HashMap<>();
+        for (Dimension dimension : dimensions) {
+            jsonDimensions.put(dimension.getKey(),dimension.getValue());
+        }
+        return jsonDimensions;
+    }
+
+    public Map<String, Number> getMetricMap() {
+        Map<String, Number> metric = new HashMap<>();
+        metric.put(this.name,this.value);
+        return metric;
     }
 
     public void addDimensionsToStart(List<Dimension> dimensionToAdd) {
