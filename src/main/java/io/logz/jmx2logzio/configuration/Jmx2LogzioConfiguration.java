d92ee72b8f14743e3deb650f7b811da83ad1c228
yyyogev <yogev.metzuyanim@logz.io>
2020-01-26 15:14:31 +0200

    import
    Signed-off-by: yyyogev <yogev.metzuyanim@logz.io>

--- src/main/java/io/logz/jmx2logzio/configuration/Jmx2LogzioConfiguration.java
+++ src/main/java/io/logz/jmx2logzio/configuration/Jmx2LogzioConfiguration.java
@@ -21,6 +21,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.Objects;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
